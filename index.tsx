8a3be72247f72aebca55312497e7df8a985db786
tomerghelber <tomergelber@gmail.com>
2021-01-30 15:36:20 +0200

    1 binaries uploaded

--- index.tsx
+++ index.tsx
@@ -1,18 +1,30 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import Hello from './Hello';
-import './style.css';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import Hello from "./Hello";
+import "./style.css";
 
-interface AppProps { }
+interface AppProps {}
 interface AppState {
   name: string;
 }
+
+import { Cards } from "react-responsive-cards";
+
+const details = [
+  {
+    title: "The Card Title",
+    description: "This is a short description",
+    image: "https://<image_here>.jpg",
+    renderFooter: <div>{"Custom JSX"}</div>,
+    handleOnClick: () => alert("Custom Event")
+  }
+];
 
 class App extends Component<AppProps, AppState> {
   constructor(props) {
     super(props);
     this.state = {
-      name: 'React'
+      name: "React"
     };
   }
 
@@ -20,52 +32,11 @@ class App extends Component<AppProps, AppState> {
     return (
       <div>
         <Hello name={this.state.name} />
-        <p>
-          Start editing to see some magic happen :)
-        </p>
+        <p>Start editing to see some magic happen :)</p>
+        <Cards details={details} />
       </div>
     );
   }
 }
 
-render(<App />, document.getElementById('root'));
-
-import dropboxV2Api from 'dropbox-v2-api';
-
-const dropbox = dropboxV2Api.authenticate({
-    client_id: 'APP_KEY',
-    client_secret: 'APP_SECRET',
-    redirect_uri: 'REDIRECT_URI',
-    state: 'OPTIONAL_STATE_VALUE'
-});
-//generate and visit authorization sevice
-const authUrl = dropbox.generateAuthUrl();
-//after redirection, you should receive code
-dropbox.getToken(code, (err, result, response) => {
-    //you are authorized now!
-    //your optional state, given in authenticate: response.query.state
-});
-
-interface Drive {
-
-}
-
-class DropBoxV2ApiDrive implements Drive {
-  private dropbox: any;
-
-  public constructor(sedropboxssion: any) {
-    this.dropbox = dropbox;
-  }
-
-  public dir(path: string, successCallback: (result: any) => void, failureCallback:(err: string) => void) {
-    this.dropbox({
-        resource: 'files/list_folder',
-        parameters: {
-          "path": path,
-      }
-    }, (err: any, result: any, response: any) => {
-        if(err){ failureCallback(err); }
-        else { successCallback(result); }
-    });
-  }
-}
+render(<App />, document.getElementById("root"));
