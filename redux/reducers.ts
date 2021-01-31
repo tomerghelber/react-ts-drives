45130e14069771382e5858f4f545ce818ed30e7f
tomerghelber <tomergelber@gmail.com>
2021-01-31 12:10:12 +0200

    Example of reducer

--- redux/reducers.ts
+++ redux/reducers.ts
@@ -1,14 +1,33 @@
+import {
+  createAction,
+  createReducer,
+  AnyAction,
+  PayloadAction
+} from "@reduxjs/toolkit";
+
 const initialState = {};
 
-interface Action {
-  type: string
+const increment = createAction<number>("increment");
+const decrement = createAction<number>("decrement");
+
+function isActionWithNumberPayload(
+  action: AnyAction
+): action is PayloadAction<number> {
+  return typeof action.payload === "number";
 }
 
-export default (state = initialState, action: Action) => {
-  switch(action.type) {
-    default:
-      // If this reducer doesn't recognize the action type, or doesn't
-      // care about this specific action, return the existing state unchanged
-      return state;
-  }
-};
+export default createReducer(initialState, builder => {
+  builder
+    .addCase(increment, (state, action) => {
+      // action is inferred correctly here
+      state.counter += action.payload;
+    })
+    // You can chain calls, or have separate `builder.addCase()` lines each time
+    .addCase(decrement, (state, action) => {
+      state.counter -= action.payload;
+    })
+    // You can apply a "matcher function" to incoming actions
+    .addMatcher(isActionWithNumberPayload, (state, action) => {})
+    // and provide a default case if no other handlers matched
+    .addDefaultCase((state, action) => {});
+});
